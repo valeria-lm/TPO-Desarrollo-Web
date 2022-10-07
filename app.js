fdf46f3cce94f0063813bc42fcf31de7c1bbcb9e
Valeria Lopez <vplopezm@gmail.com>
2022-10-06 23:53:16 -0300

    add

--- app.js
+++ app.js
@@ -7,3 +7,9 @@ const year = date.getFullYear();
 
 // insert year to html
 copyright.innerHTML = `© ${year}. Copyright Tienda AVA Café. Todos los derechos reservados`;
+
+/* ---------------- */
+window.addEventListener("scroll", function(){
+  let nav = document.querySelector("nav");
+  nav.classList.toggle("abajo", window.scrollY>0);
+})
