98f7541b88d71083c6e0f058ba31ad5d69aea26c
Valeria Lopez <vplopezm@gmail.com>
2022-10-12 23:25:59 -0300

    new changes

--- app.js
+++ app.js
@@ -1,17 +1,43 @@
-// variables html
 const copyright = document.querySelector("#copyright");
-
-// create year
 const date = new Date();
 const year = date.getFullYear();
 
-// insert year to html
+// insertar año en html
 copyright.innerHTML = `© ${year}. Copyright Tienda AVA Café. Todos los derechos reservados`;
 
-/* ---------------- */
-// change nav color, scroll
-window.addEventListener("scroll", function(){
-  let nav = document.querySelector("nav");
-  nav.classList.toggle("abajo", window.scrollY>0);
-})
+/* ------------------------------ */
 
+// cambiar color del nav al hacer scroll
+window.addEventListener("scroll", function () {
+  let nav = document.querySelector("nav");
+  nav.classList.toggle("nav-scroll", window.scrollY > 0);
+});
+
+/* ------------------------------ */
+
+// validación form: contacto.html
+const nombreApellido = document.getElementById("nombre-y-apellido");
+const correoElectronico = document.getElementById("correo-electronico");
+const telefono = document.getElementById("telefono");
+const form = document.getElementById("form");
+const errorElement = document.getElementById("error");
+
+form.addEventListener("submit", (e) => {
+  let messages = [];
+  if (nombreApellido.value === "" || nombreApellido.value == null) {
+    messages.push("*Es necesario su nombre y apellido");
+  }
+
+  if (correoElectronico.value === "" || correoElectronico.value == null) {
+    messages.push("*Es necesario su mail");
+  }
+
+  if (telefono.value === "" || telefono.value == null) {
+    messages.push("*Es necesario su número de telefono");
+  }
+
+  if (messages.length > 0) {
+    e.preventDefault();
+    errorElement.innerText = messages.join(", ");
+  }
+});
